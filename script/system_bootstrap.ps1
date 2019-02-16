0c1f89054731e70538c98f2178c7543eb09ff83a
aJchemist <1694505+aJchemist@users.noreply.github.com>
2019-02-16 17:03:39 +0900

    Fixed script/system_bootstrap.ps1

--- script/system_bootstrap.ps1
+++ script/system_bootstrap.ps1
@@ -5,12 +5,12 @@ $ErrorActionPreference = "Stop"
 
 
 $selfdir = [System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)
-$moduledir = (Get-Item $selfdir).parent.parent
+$parentdir = (Get-Item $selfdir).parent
 
 
 $DataDrive = Get-PSDrive -PSProvider FileSystem | Out-GridView -PassThru
 $DebugDir = (New-Item -ItemType directory -Path $DataDrive -Name "\system_bootstrap" -Force).FullName
 
 
-Import-Module "$moduledir\core\profile_list.ps1"
-Import-Module "$moduledir\core\policies.ps1"
+Import-Module "$parentdir\core\profile_list.ps1"
+Import-Module "$parentdir\core\policies.ps1"
