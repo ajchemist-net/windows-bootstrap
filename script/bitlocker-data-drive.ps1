44b37c28cab235345c363350721e80f8c6ddeb80
ajchemist <1694505+ajchemist@users.noreply.github.com>
2021-04-21 22:11:53 +0900

    Update bitlocker-data-drive.ps1

--- script/bitlocker-data-drive.ps1
+++ script/bitlocker-data-drive.ps1
@@ -12,7 +12,7 @@ Enable-BitLocker $DataDrive.root -UsedSpaceOnly -PasswordProtector -Password $da
 $data_volume = Add-BitLockerKeyProtector $DataDrive.root -RecoveryPasswordProtector
 $data_recovery_pw = $data_volume.KeyProtector | Where-Object { $_.KeyProtectorType -eq "RecoveryPassword" } | Select-Object -ExpandProperty RecoveryPassword
 $startScriptContent = [string]::Format("Unlock-BitLocker {0} -RecoveryPassword {1}", $DataDrive.root, $data_recovery_pw)
-mkdir -p $env:SystemRoot\System32\GroupPolicy\Machine\Scripts\Startup
+mkdir -Force $env:SystemRoot\System32\GroupPolicy\Machine\Scripts\Startup
 Add-Content -Path "$env:SystemRoot\System32\GroupPolicy\Machine\Scripts\Startup\unlock_bitlocker_data_drive.ps1" -Value $startScriptContent
 
 
